8cd3b593a47c1018c33ae872d0c2d16ae02d1035
Ero-2 <125475081+Ero-2@users.noreply.github.com>
2023-11-09 00:55:15 -0600

    Agregar archivos de proyecto.

---
+++ Bicola de entrada restringida/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bicola_de_entrada_restringida
+{
+
+    class InputRestrictedDeque<T>
+    {
+        private LinkedList<T> deque = new LinkedList<T>();
+
+        public void AddRear(T item)
+        {
+            deque.AddLast(item);
+        }
+
+        public T RemoveFront()
+        {
+            if (deque.Count == 0)
+            {
+                throw new InvalidOperationException("La bicola está vacía.");
+            }
+
+            T item = deque.First.Value;
+            deque.RemoveFirst();
+            return item;
+        }
+
+        public int Count()
+        {
+            return deque.Count;
+        }
+    }
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InputRestrictedDeque<int> irDeque = new InputRestrictedDeque<int>();
+
+            irDeque.AddRear(1);
+            irDeque.AddRear(2);
+            irDeque.AddRear(3);
+
+            Console.WriteLine("Elemento frontal removido: " + irDeque.RemoveFront());
+            Console.WriteLine("Número de elementos en la bicola: " + irDeque.Count());
+
+            Console.ReadKey();
+
+        }
+    }
+}
